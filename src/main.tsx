8b6958d908fc2585398f0c52458254524ecddb46
MaetDol <rndka9@gmail.com>
2024-11-03 10:11:25 +0900

    feat, 기본 스타일 파일들 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import 'normalize.css';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import "./styles/reset.css";
+import "normalize.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./styles/default.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
